4df2d0496953680e9fe05f31554c769813c987d0
sunnyskykinglive <161257199+sunnyskykinglive@users.noreply.github.com>
2024-02-26 01:46:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.querySelectorAll('.btn')
 console.log(btn)
 const playSong = () =>{
   const song = new Audio('/Sunnyskyking_Awesome_praise.mp3')
-  console.log(Sunnyskyking.mp3)
+  console.log(sunnyskyking.mp3)
   song.play()
 alert('A button was clicked')
 }
